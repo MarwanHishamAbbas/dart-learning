e73c00daa70708c5c114260d8361c8b9734531cb
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2022-11-15 17:27:01 +0200

    null safety

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,13 +1,29 @@
-import "dart:io";
+// ? Operator
+/// the null-aware operators make it much easier. The following operator basically says, “Hey, assign userAge to user.age. But if the user object is null, that’s okay. Just assign userAge to null, rather than throwing an error”:
+///
+void getUserAge(String username) async {
+  final request = UserRequest(username);
+  final response = await request.get();
+  User user = new User.fromResponse(response);
+  this.userAge = user?.age;
 
-void main(List<String> args) {
-  // stdout.writeIn is functionally the same as print but can also be used to write text to files
-  stdout.writeln("Greet Somebody");
-  // readLineSync is a blocking function that stops execution and waits for the user to respond in the command line.
-  String? input = stdin.readLineSync();
-  return helloDart(input!);
+  /// NOTE It’s worth pointing out that if any code below the line this.userAge = user?.age; relied on useAge not being null, the result would be an error.
 }
 
-void helloDart(String name) {
-  print('Hello, $name');
+// ?? Operator
+/// This operator says, “Hey program, do this operation with this value or variable. But if that value or variable is null, then use this backup value.” It allows you to assign a default value at any given point in your process, and it’s super handy:
+/// 
+void getUserAge(String username) async {
+  final request = new UserRequest(username);
+  final response = request.get();
+  Useruser = new User.fromResponse(response);
+  this.userAge = user.age ?? 18;
+  // etc.
 }
+
+
+// THE ??= OPERATOR
+/// This operator basically says, “Hey, if this object is null, then assign it to this value. If it’s not, just return the object as is”:
+
+int x = 5;
+x ??= 3;
