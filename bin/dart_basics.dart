a512490736ec62a6cd8165908c5320b71a9c1caf
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2022-11-15 16:34:59 +0200

    for in loops

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,1 +1,11 @@
+void main() {
+  List<String> greetings = ['Earth', 'Mars', 'Pluto'];
 
+  for (var name in greetings) {
+    helloDart(name);
+  }
+}
+
+void helloDart(String name) {
+  print('Hello, $name');
+}
