09c11b8287ff4d09ff68bf3d7f906ce5dc9f4b24
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2022-11-16 12:23:41 +0200

    inheritance example

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,1 +1,17 @@
+// Inheritance
 
+class Animal {
+  late String name;
+  late int legCount;
+}
+
+class Cat extends Animal {
+  void makeNoise() {
+    print("Merrrrr");
+  }
+}
+
+void main(List<String> args) {
+  Cat cat = Cat();
+  cat.makeNoise();
+}
