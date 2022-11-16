d91f5d01dcfc84f173d0daf0c13864f22657d99d
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2022-11-16 11:54:35 +0200

    Lexical scope

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,28 +1,20 @@
-void main(List<String> args) {
-// Positional parameters
-  void debugger(String message, int lineNum) {}
-  debugger("A bug!", 55);
-
-// Named parameters
-  void anotherDebugger({required String message, required int lineNum}) {}
-
-  /// OR
-  /// void anotherDebugger({String? message, int? lineNum}) {}
-  anotherDebugger(message: "A bug!", lineNum: 32);
-
-// Positional optional parameters
-  int addSomeNums(int x, int y, [int? z]) {
-    int sum = x + y;
-    if (z != null) {
-      sum += z;
+String topLevel = 'Hello';
+void firstFunction() {
+  String secondLevel = 'Hi';
+  print(topLevel);
+  nestedFunction() {
+    String thirdLevel = 'Howdy';
+    print(topLevel);
+    print(secondLevel);
+    innerNestedFunction() {
+      print(topLevel);
+      print(secondLevel);
+      print(thirdLevel);
     }
-    return sum;
   }
 
-  addSomeNums(2, 3, 10);
-
-// Default parameters values
-  void addAnotherNums(int x, int y, [int z = 5]) => {x + y + z};
-
-  //
+// Error (outside the scope)
+  print(thirdLeve);
 }
+
+void main(List<String> args) {}
